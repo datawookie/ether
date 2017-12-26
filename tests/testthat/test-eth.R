e69f3e136025f2c2edf8b664ad9ee80e6c033ecf
Andrew B. Collier <andrew@exegetic.biz>
2017-12-26 10:35:58 +0200

    Set RPC address in test.

--- tests/testthat/test-eth.R
+++ tests/testthat/test-eth.R
@@ -1,7 +1,10 @@
 library(ethereum)
 
+set_rpc_address("https://mainnet.infura.io/", key = Sys.getenv("INFURA_MAINNET_KEY"))
+
 context("eth")
 
 test_that("valid protocol version", {
+  print(get_rpc_address())
   expect_type(eth_protocolVersion(), "integer")
 })
