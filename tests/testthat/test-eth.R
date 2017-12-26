7a6637401da476a68cfd2dd264ca48663ef6631e
Andrew B. Collier <andrew@exegetic.biz>
2017-12-26 19:20:09 +0200

    Added test stub.

--- tests/testthat/test-eth.R
+++ tests/testthat/test-eth.R
@@ -8,3 +8,12 @@ test_that("valid protocol version", {
   print(get_rpc_address())
   expect_type(eth_protocolVersion(), "integer")
 })
+
+test_that("can retrieve transactions", {
+  eth_getTransactionByBlockHashAndIndex("0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1", "0x0")
+  #
+  # MAKE THIS INTO A TEST!!
+  # MAKE THIS INTO A TEST!!
+  # MAKE THIS INTO A TEST!!
+  # MAKE THIS INTO A TEST!!
+})
