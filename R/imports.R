707bc84a948dda71ecc08c37c4432a3b39f5e90a
Andrew B. Collier <andrew@exegetic.biz>
2017-12-27 06:38:03 +0200

    Fixed dependencies.

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,8 @@
 #' @importFrom magrittr %>%
 #' @name %>%
 #' @importFrom Rmpfr mpfr
+#' @importFrom jsonlite toJSON
+#' @importFrom httr POST
+#' @importFrom httr content
+#' @importFrom httr content_type_json
 NULL
