d884d9106ff931e7e0dbabeed2fefce5230301e8
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 05:31:42 +0200

    hex_to_number() converts big hex to either (mpfr) integer or numeric.

--- R/util.R
+++ R/util.R
@@ -1,6 +1,18 @@
-hex_to_number <- function(hex) {
-  ifelse(grepl("^0[xX]", hex), hex, paste0("0x", hex)) %>%
-    strtoi()
+hex_to_number <- function(hex, integer = TRUE) {
+  hex <- ifelse(grepl("^0[xX]", hex), hex, paste0("0x", hex))
+  #
+  number <- strtoi(hex)
+  #
+  number <- ifelse(is.na(number),
+                   if (integer) {
+                     mpfr(hex, base = 16)
+                   } else {
+                     class(hex) <- "numeric"
+                     as.numeric(sprintf("%.0f", hex))
+                   },
+                   number)
+  #
+  number
 }
 
 number_to_hex <- function(number) {
