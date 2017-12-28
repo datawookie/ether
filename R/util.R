957918c12ef951031c4e65ff4d90ed881465452d
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 18:52:29 +0200

    Documented utility functions. Don't run examples.

--- R/util.R
+++ R/util.R
@@ -19,15 +19,46 @@ number_to_hex <- function(number) {
   sprintf("0x%x", number)
 }
 
+#' Convert Wei to kWei.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
-as.kwei   <- function(wei) wei / 1000
+as.kwei   <- function(wei) {
+  wei / 1000
+}
+
+#' Convert Wei to MWei.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
 as.mwei   <- function(wei) wei / 1000000
+
+#' Convert Wei to GWei.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
 as.gwei   <- function(wei) wei / 1000000000
+
+#' Convert Wei to Szabo.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
 as.szabo  <- function(wei) wei / 1000000000000
+
+#' Convert Wei to Finney.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
 as.finney <- function(wei) wei / 1000000000000000
+
+#' Convert Wei to Ether.
+#'
+#' @param wei Number of Wei.
+#'
 #' @export
 as.ether  <- function(wei) wei / 1000000000000000000
