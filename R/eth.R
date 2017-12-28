e526e6b235767e3338a559d32e27daf893cff872
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 17:44:38 +0200

    Don't run examples.

--- R/eth.R
+++ R/eth.R
@@ -235,9 +235,11 @@ eth_getUncleCountByBlockNumber <- function(number) {
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' eth_getBlock()
 #' eth_getBlock("0xb6d656ead4c3d4b1aa24d6b4d3d4cde8c090794e597258993512d650f088fcba")
 #' eth_getBlock("0x4720FF")
+#' }
 eth_getBlock <- function(hash = NULL, number = "latest", full = TRUE) {
   if (!is.null(hash))
     block <- get_post_response("eth_getBlockByHash", list(hash, full))
