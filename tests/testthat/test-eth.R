f24eb424de54af4ea9c6696b4dc722e6932cfc0f
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 10:17:27 +0200

    Fixed checks on eth_getTransaction*.

--- tests/testthat/test-eth.R
+++ tests/testthat/test-eth.R
@@ -5,12 +5,14 @@ set_rpc_address("https://mainnet.infura.io/", key = Sys.getenv("INFURA_MAINNET_K
 context("eth")
 
 test_that("valid protocol version", {
-  print(get_rpc_address())
   expect_type(eth_protocolVersion(), "integer")
 })
 
 test_that("can retrieve transactions", {
-  eth_getTransactionByBlockHashAndIndex("0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1", "0x0")
+  tx = eth_getTransactionByBlockHashAndIndex("0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1", "0x0")
+  expect_equal(tx$blockNumber, "0x493c5f")
+  tx = eth_getTransactionByBlockNumberAndIndex("0x493c5f", "0x0")
+  expect_equal(tx$blockHash, "0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1")
   #
   # MAKE THIS INTO A TEST!!
   # MAKE THIS INTO A TEST!!
