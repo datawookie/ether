ce10b126fcdde4eaf9c1ead0b9e2ccff89744498
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 10:16:49 +0200

    Code to fix R CMD check.

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,13 @@
 #' @importFrom magrittr %>%
 #' @name %>%
+#' @importFrom dplyr bind_rows
 #' @importFrom Rmpfr mpfr
 #' @importFrom jsonlite toJSON
 #' @importFrom httr POST
 #' @importFrom httr content
 #' @importFrom httr content_type_json
 NULL
+#
+# Stops R CMD check from complaining "no visible binding for global variable ‘.’".
+#
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
