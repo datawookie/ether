08d428ffbdf81be6d8b41a41f251a85dedf259ef
Andrew B. Collier <andrew@exegetic.biz>
2017-12-28 09:50:59 +0200

    Added web3 functions.

--- R/web3.R
+++ R/web3.R
@@ -1,2 +1,25 @@
-# web3_clientVersion
-# web3_sha3
+#' Returns the current client version.
+#'
+#' @return String.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' web3_clientVersion()
+#' }
+web3_clientVersion <- function() {
+  get_post_response("web3_clientVersion")
+}
+
+#' Returns Keccak-256 (not the standardized SHA3-256) of the given data.
+#'
+#' @return String.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' web3_sha3("0x68656c6c6f20776f726c64")
+#' }
+web3_sha3 <- function(data) {
+  get_post_response("web3_sha3", list(data))
+}
