18d0065eed01e6eef219d086d30362e01704b671
Andrew B. Collier <andrew@exegetic.biz>
2017-12-29 05:54:59 +0200

    Package documentation.

--- R/ethereum.R
+++ R/ethereum.R
@@ -1,3 +1,10 @@
+#' ethereum: An R package for working with Ethereum.
+#'
+#' The ethereum package provides an interface to the Ethereum RPC functions.
+#'
+#' @docType package
+#' @name ethereum
+#'
 #' @import dplyr
 #' @importFrom Rmpfr mpfr
 #' @importFrom jsonlite toJSON
@@ -5,7 +12,7 @@
 #' @importFrom httr content
 #' @importFrom httr content_type_json
 NULL
-#
+
 # Stops R CMD check from complaining "no visible binding for global variable ‘.’".
 #
 if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
