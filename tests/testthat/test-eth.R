2f1ddd7752ee76c3f9e4329824e099e095658b82
Andrew Collier <andrew@exegetic.biz>
2020-01-24 20:09:26 +0200

    Use httptest for testing

--- tests/testthat/test-eth.R
+++ tests/testthat/test-eth.R
@@ -2,13 +2,17 @@ set_rpc_address("https://mainnet.infura.io/", key = Sys.getenv("INFURA_MAINNET_K
 
 context("eth")
 
-test_that("valid protocol version", {
-  expect_type(eth_protocolVersion(), "integer")
+with_mock_api({
+  test_that("valid protocol version", {
+    expect_type(eth_protocolVersion(), "integer")
+  })
 })
 
-test_that("can retrieve transactions", {
-  tx = eth_getTransactionByBlockHashAndIndex("0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1", "0x0")
-  expect_equal(tx$blockNumber, "0x493c5f")
-  tx = eth_getTransactionByBlockNumberAndIndex("0x493c5f", "0x0")
-  expect_equal(tx$blockHash, "0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1")
+with_mock_api({
+  test_that("can retrieve transactions", {
+    tx = eth_getTransactionByBlockHashAndIndex("0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1", "0x0")
+    expect_equal(tx$blockNumber, "0x493c5f")
+    tx = eth_getTransactionByBlockNumberAndIndex("0x493c5f", "0x0")
+    expect_equal(tx$blockHash, "0xd3ca9f0659473d93a00dc2076c2c9e8d7805243c59c8cb0d3501b262f98a15f1")
+  })
 })
